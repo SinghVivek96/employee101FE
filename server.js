68d6b2daf6e13fe288cf454a2ed9d6b6ea8de24b
VIVEK SINGH <itsvivek96@gmail.com>
2021-07-18 12:51:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/employee101FE'));
+app.use(express.static('./dist/employee101FE'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/employee101FE/'}),
