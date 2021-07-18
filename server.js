4beb657c9380331cd0cf7e9580b99cbda7aabf60
VIVEK SINGH <itsvivek96@gmail.com>
2021-07-18 12:43:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/employee101FE'));
+// app.use(express.static('./dist/employee101FE'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/employee101FE/'}),
 );
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS','PATCH');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
